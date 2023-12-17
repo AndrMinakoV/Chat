b0dde7d49aaad55c5b6bd350e7663e316faaf924
Andrey <borishasanov591@gmail.com>
2023-12-17 15:01:26 +0200

    for tiktok

--- src/main/java/com/mecheniy/chat/Chat.java
+++ src/main/java/com/mecheniy/chat/Chat.java
@@ -6,20 +6,51 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import com.mecheniy.chat.utilities.MessageFunctions;
-
-import java.util.Iterator;
-import java.util.List;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod("chat")
 public class Chat {
     public static final String MODID = "chat";
-    public Chat() {
-    }
+        @Mod.EventBusSubscriber(modid = Chat.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+        public static class CommandRegistry {
+            @SubscribeEvent
+            public static void onRegisterCommands(RegisterCommandsEvent event) {
+                // Регистрируем новую команду
+                event.getDispatcher().register(Commands.literal("kit")
+                        .then(Commands.literal("penis")
+                                .executes(context -> kitPenis(context.getSource()))));
+            }
+
+            private static int kitPenis(CommandSourceStack source) {
+                if (source.getEntity() instanceof ServerPlayer) {
+                    ServerPlayer player = (ServerPlayer) source.getEntity();
+                    // Создаем новый предмет (камень)
+                    ItemStack itemStack = new ItemStack(Items.STONE, 64); // 64 камня
+
+                    // Проверяем, есть ли место в инвентаре
+                    if (player.getInventory().add(itemStack)) {
+                        // Уведомляем игрока о получении предмета
+                        source.sendSuccess(Component.literal("§aВы получили 64 камня!"), true);
+                    } else {
+                        // Если нет места, сообщаем об этом
+                        source.sendFailure(Component.literal("§cНедостаточно места в инвентаре для камней."));
+                    }
+                } else {
+                    source.sendFailure(Component.literal("§cЭту команду может использовать только игрок."));
+                }
+                return 1; // Возвращаем успешный результат выполнения команды
+            }
     @Mod.EventBusSubscriber(modid = Chat.MODID)
     public static class ForgeBeautifulChatEvent {
 
@@ -51,5 +82,5 @@ public class Chat {
 
 
     }
-
+}
 }
