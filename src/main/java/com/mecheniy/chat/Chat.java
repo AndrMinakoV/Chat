9a87371b0a74598c80ebeea0125aecf9500c6350
Aisssaev <miroslavzizyn2006@gmail.com>
2023-12-17 13:47:50 +0200

    Local chat

--- src/main/java/com/mecheniy/chat/Chat.java
+++ src/main/java/com/mecheniy/chat/Chat.java
@@ -4,12 +4,16 @@ import ca.weblite.objc.Client;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import com.mecheniy.chat.utilities.MessageFunctions;
+
+import java.util.Iterator;
+import java.util.List;
 
 @Mod("chat")
 public class Chat {
@@ -18,21 +22,30 @@ public class Chat {
     }
     @Mod.EventBusSubscriber(modid = Chat.MODID)
     public static class ForgeBeautifulChatEvent {
+
+        public static final double rangeTalk = 100;
         @SubscribeEvent
         public static void onServerChat(ServerChatEvent event) {
-            System.out.println("Test suc");
                 ServerPlayer serverPlayer = event.getPlayer();
                 MinecraftServer server = serverPlayer.getServer();
                 String playerName = serverPlayer.getName().getString();
                 String rawMessage = event.getMessage().getString();
+
+
                 event.setCanceled(true);
                 if (rawMessage.startsWith("!")){
 
                     Component formattedMessage = Component.literal("§8[§6G§8] [§c" + playerName + "§8] ").append(Component.literal(rawMessage.substring(1)));
                     MessageFunctions.broadcastMessage(server, formattedMessage);
                 } else {
-                    Component formattedMessage = Component.literal("§8[§aL§8] [§c" + playerName + "§8] ").append(Component.literal(rawMessage));
-                    MessageFunctions.broadcastMessage(server, formattedMessage);
+                    for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                        if (MessageFunctions.compareCoordinateDistance(serverPlayer.getOnPos(), player.getOnPos()) <= rangeTalk){
+                            Component formattedMessage = Component.literal("§8[§aL§8] [§c" + playerName + "§8] ").append(Component.literal(rawMessage));
+                            MessageFunctions.broadcastMessage(server, formattedMessage);
+                        }
+                    }
+
+
                 }
 
 
--- src/main/java/com/mecheniy/chat/utilities/MessageFunctions.java
+++ src/main/java/com/mecheniy/chat/utilities/MessageFunctions.java
@@ -1,5 +1,6 @@
 package com.mecheniy.chat.utilities;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
@@ -10,6 +11,13 @@ public class MessageFunctions {
             player.sendSystemMessage(message);
 
         }
+    }
+
+    public static double compareCoordinateDistance(BlockPos playerPos1, BlockPos playerPos2){
+        double x = (double) Math.abs(playerPos1.getX() - playerPos2.getX());
+        double y = (double) Math.abs(playerPos1.getY() - playerPos2.getY());
+        double z = (double) Math.abs(playerPos1.getZ() - playerPos2.getZ());
+        return x + y + z;
     }
 
 
