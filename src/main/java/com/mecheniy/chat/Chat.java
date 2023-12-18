781d3d0764eb39389fd4f052cbb0a34d7bdc3909
Andrey <borishasanov591@gmail.com>
2023-12-18 18:21:36 +0200

    timestamp added

--- src/main/java/com/mecheniy/chat/Chat.java
+++ src/main/java/com/mecheniy/chat/Chat.java
@@ -8,6 +8,9 @@ import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import com.mecheniy.chat.utilities.MessageFunctions;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 
 @Mod("chat")
 public class Chat {
@@ -17,16 +20,19 @@ public class Chat {
             public static final double rangeTalk = 100;
             @SubscribeEvent
             public static void onServerChat(ServerChatEvent.Submitted event) {
+                LocalTime timeNow = LocalTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                String formattedTime = timeNow.format(formatter);
                 ServerPlayer serverPlayer = event.getPlayer();
                 MinecraftServer server = serverPlayer.getServer();
                 String playerName = serverPlayer.getName().getString();
                 String rawMessage = event.getMessage().getString();
                 event.setCanceled(true);
                 if (rawMessage.startsWith("!")) {
-                    Component formattedMessage = Component.literal("§8[§6G§8] [§c" + playerName + "§8] ").append(Component.literal(rawMessage.substring(1)));
+                    Component formattedMessage = Component.literal("§8[" + formattedTime + "] [§6G§8] §c" + playerName + "§8: ").append(Component.literal(rawMessage.substring(1)));
                     MessageFunctions.broadcastMessageGlobal(server, formattedMessage);
                 } else {
-                    Component formattedMessage = Component.literal("§8[§aL§8] [§c" + playerName + "§8] ").append(Component.literal(rawMessage));
+                    Component formattedMessage = Component.literal("§8[" + formattedTime + "] [§aL§8] §c" + playerName + "§8: ").append(Component.literal(rawMessage));
                     MessageFunctions.broadcastMessageLocal(serverPlayer, formattedMessage);
                 }
             }
