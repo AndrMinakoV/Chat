a6d71c1642b36ba336663c5812db7101f5e55bef
AndrMinakoV <borishasanov591@gmail.com>
2023-12-18 18:44:36 +0200

    123

--- src/main/java/com/mecheniy/chat/Chat.java
+++ src/main/java/com/mecheniy/chat/Chat.java
@@ -1,9 +1,11 @@
 package com.mecheniy.chat;
 
-
+import ca.weblite.objc.Client;
+import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,11 +17,13 @@ import java.time.format.DateTimeFormatter;
 @Mod("chat")
 public class Chat {
     public static final String MODID = "chat";
-        @Mod.EventBusSubscriber(modid = Chat.MODID)
-        public static class ForgeBeautifulChatEvent {
-            public static final double rangeTalk = 100;
-            @SubscribeEvent
-            public static void onServerChat(ServerChatEvent.Submitted event) {
+    public Chat() {
+    }
+    @Mod.EventBusSubscriber(modid = Chat.MODID)
+    public static class ForgeBeautifulChatEvent {
+        @SubscribeEvent
+        public static void onServerChat(ServerChatEvent.Submitted event) {
+
                 LocalTime timeNow = LocalTime.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
                 String formattedTime = timeNow.format(formatter);
@@ -28,15 +32,22 @@ public class Chat {
                 String playerName = serverPlayer.getName().getString();
                 String rawMessage = event.getMessage().getString();
                 event.setCanceled(true);
-                if (rawMessage.startsWith("!")) {
+                if (rawMessage.startsWith("!")){
+
                     Component formattedMessage = Component.literal("§8[" + formattedTime + "] [§6G§8] §c" + playerName + "§8: ").append(Component.literal(rawMessage.substring(1)));
                     MessageFunctions.broadcastMessageGlobal(server, formattedMessage);
                 } else {
                     Component formattedMessage = Component.literal("§8[" + formattedTime + "] [§aL§8] §c" + playerName + "§8: ").append(Component.literal(rawMessage));
-                    MessageFunctions.broadcastMessageLocal(serverPlayer, formattedMessage);
+                    MessageFunctions.broadcastMessageLocal(serverываыва, formattedMessage);
                     //11
                 }
+
+
+
+
             }
-        }
+
+
     }
 
+}
