d0c24e39105ca911d73c5b2e7cd0456a83aef6fc
AndrMinakoV <borishasanov591@gmail.com>
2023-12-23 19:15:20 +0200

    logs added

--- src/main/java/com/mecheniy/chat/Chat.java
+++ src/main/java/com/mecheniy/chat/Chat.java
@@ -1,6 +1,5 @@
 package com.mecheniy.chat;
 
-
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.model.user.User;
@@ -13,44 +12,56 @@ import net.minecraftforge.fml.common.Mod;
 import com.mecheniy.chat.utilities.MessageFunctions;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
-
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 @Mod("chat")
 public class Chat {
     public static final String MODID = "chat";
-        @Mod.EventBusSubscriber(modid = Chat.MODID)
-        public static class ForgeBeautifulChatEvent {
-            public static final double rangeTalk = 100;
-            @SubscribeEvent
-            public static void onServerChat(ServerChatEvent.Submitted event) {
-                ServerPlayer serverPlayer = event.getPlayer();
-                User user = LuckPermsProvider.get().getUserManager().getUser(serverPlayer.getUUID());
-                String prefix = "";
-                if (user != null) {
-                    CachedMetaData metaData = user.getCachedData().getMetaData();
-                    prefix = metaData.getPrefix();
-                    if (prefix == null) {
-                        prefix = "";
-                    }
-                }
-                LocalTime timeNow = LocalTime.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-                String formattedTime = timeNow.format(formatter);
+    private static final Logger LOGGER = Logger.getLogger(MODID);
 
-                MinecraftServer server = serverPlayer.getServer();
-                String playerName = serverPlayer.getName().getString();
-                String rawMessage = event.getMessage().getString();
-                event.setCanceled(true);
-                if (rawMessage.startsWith("!")) {
-                    Component formattedMessage = Component.literal("§8[" + "§7" +formattedTime + "§8] " + "[§6G§8] "  +  prefix  + " §7" + playerName + "§8: ").append(Component.literal(rawMessage.substring(1)));
-                    MessageFunctions.broadcastMessageGlobal(server, formattedMessage);
-                } else {
-                    Component formattedMessage = Component.literal("§8[" + "§7" +formattedTime + "§8] " + "[§aL§8] "  +  prefix  + " §7" + playerName + "§8: ").append(Component.literal(rawMessage));
-                    MessageFunctions.broadcastMessageLocal(serverPlayer, formattedMessage);
+    @Mod.EventBusSubscriber(modid = Chat.MODID)
+    public static class ForgeBeautifulChatEvent {
+        public static final double rangeTalk = 100;
 
+        @SubscribeEvent
+        public static void onServerChat(ServerChatEvent.Submitted event) {
+            ServerPlayer serverPlayer = event.getPlayer();
+
+             /*User user = LuckPermsProvider.get().getUserManager().getUser(serverPlayer.getUUID());
+            String prefix = "";
+            if (user != null) {
+                CachedMetaData metaData = user.getCachedData().getMetaData();
+                prefix = metaData.getPrefix();
+                if (prefix == null) {
+                    prefix = "";
                 }
+            } */
+
+            LocalTime timeNow = LocalTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formattedTime = timeNow.format(formatter);
+
+            MinecraftServer server = serverPlayer.getServer();
+            String playerName = serverPlayer.getName().getString();
+            String rawMessage = event.getMessage().getString();
+            event.setCanceled(true);
+            String chatTag;
+            Component formattedMessage;
+            if (rawMessage.startsWith("!")) {
+                chatTag = "G";
+                // Убираем первый символ для глобального чата
+                String messageWithoutFirstChar = rawMessage.substring(1);
+                formattedMessage = Component.literal("§8[" + "§7" + formattedTime + "§8] " + "[§6" + chatTag + "§8] "  +  /* prefix  + */ " §7" + playerName + "§8: ").append(Component.literal(messageWithoutFirstChar));
+                MessageFunctions.broadcastMessageGlobal(server, formattedMessage);
+                // Логирование сообщения без первого символа
+                System.out.println("[" + formattedTime + "] [" + chatTag + "] " + playerName + ": " + messageWithoutFirstChar);
+            } else {
+                chatTag = "L";
+                formattedMessage = Component.literal("§8[" + "§7" + formattedTime + "§8] " + "[§a" + chatTag + "§8] "  +  /* prefix  + */ " §7" + playerName + "§8: ").append(Component.literal(rawMessage));
+                MessageFunctions.broadcastMessageLocal(serverPlayer, formattedMessage);
+                // Логирование локального сообщения
+                System.out.println("[" + formattedTime + "] [" + chatTag + "] " + playerName + ": " + rawMessage);
             }
-        }
     }
-
+}}
